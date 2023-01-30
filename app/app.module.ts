e547a85dd34ce6bfec00edf303d376c87c978565
Tarunkumarchelumalla <70788951+Tarunkumarchelumalla@users.noreply.github.com>
2023-01-30 13:17:10 +0530

    3 files modified, 1 added

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, FlexLayoutModule],
+  imports: [BrowserModule, FormsModule, FlexLayoutModule,MaterialModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
---
+++ app/material/material.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+@NgModule({
+  exports: [MatInputModule],
+  declarations: [],
+})
+export class MaterialModule {}
